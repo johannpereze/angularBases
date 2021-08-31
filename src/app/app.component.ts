59376319c082a1447e84341a3270078343a5aaf1
johannpereze <sebas.jsp@gmail.com>
2021-08-30 22:25:26 -0500

    Clase 37. mi solución

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  titulo = 'Contador App';
+  titulo: string = 'Contador App';
+  numero: number = 10;
+  contador(value:number){
+    return this.numero += value
+  }
 }
