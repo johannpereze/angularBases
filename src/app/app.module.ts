1134143365f8be2fc2cce96adc465c787f3f9d93
johannpereze <sebas.jsp@gmail.com>
2021-08-31 15:08:23 -0500

    Clase 49 despues de la tarea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
 import { HeroesModule } from './heroes/heroes.module';
+import { ContadorComponent } from './contador/contador/contador.component';
+import { ContadorModule } from './contador/contador.module';
 
+//crear modulo llamado ContadorModule
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
 
   ],
   imports: [
     BrowserModule,
-    HeroesModule
+    HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
